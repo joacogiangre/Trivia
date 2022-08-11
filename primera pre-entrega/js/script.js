b16b7f411fff1b64686025a3a145aae476eeb35b
joacogiangre <joacogiangre@gmail.com>
2022-08-10 23:26:34 -0300

    se añadieron eventos, se elimino parte de la despedida del usuario

--- primera pre-entrega/js/script.js
+++ primera pre-entrega/js/script.js
@@ -7,111 +7,111 @@ let p3;
 let r1 = "flor de cerezo";
 let r2 = "rio nilo";
 let r3 = "cordillera de los andes";
-
+const txt = document.getElementById("txt");
+const boton = document.getElementById("boton");
+const input = document.getElementById("TextInput");
+const correcta1 = document.getElementById("correcta");
 
 const infoRespuesta1 = {
-  flor:"La flor de cerezo es una flor efímera de los cerezos que florece al comienzo de la primavera, puede ser de distintos colores pero mayormente es rosa pálido. Tiene un carácter simbólico especialmente en la cultura japonesa. Su árbol tiene una altura de seis metros, y se encuentra mayormente en la zona del hemisferio norte con climas templados"
+  flor: "La flor de cerezo es una flor efímera de los cerezos que florece al comienzo de la primavera, puede ser de distintos colores pero mayormente es rosa pálido. Tiene un carácter simbólico especialmente en la cultura japonesa. Su árbol tiene una altura de seis metros, y se encuentra mayormente en la zona del hemisferio norte con climas templados",
 };
 const infoRespuesta2 = {
-  rio:"El rio Nilo es el más largo de África con una longitud de 6,650 km y una anchura de 2,8 km es el segundo río más grande del mundo. Fluye hacia el norte a través del noreste de África para desembocar en el mar Mediterráneo."
+  rio: "El rio Nilo es el más largo de África con una longitud de 6,650 km y una anchura de 2,8 km es el segundo río más grande del mundo. Fluye hacia el norte a través del noreste de África para desembocar en el mar Mediterráneo.",
 };
 
 const infoRespuesta3 = {
-    cordillera:"La cordillera de los andes se encuentra en la zona occidental de Ámerica del Sur, cuenta con una longitud de 8.500 km y una superficie de 3.371 millones de km²"
+  cordillera:
+    "La cordillera de los andes se encuentra en la zona occidental de Ámerica del Sur, cuenta con una longitud de 8.500 km y una superficie de 3.371 millones de km²",
 };
 
 const array1 = [
   infoRespuesta1,
-  { flor: "La flor de cerezo es una flor efímera de los cerezos que florece al comienzo de la primavera, puede ser de distintos colores pero mayormente es rosa pálido. Tiene un carácter simbólico especialmente en la cultura japonesa. Su árbol tiene una altura de seis metros, y se encuentra mayormente en la zona del hemisferio norte con climas templados" },
+  {
+    flor: "La flor de cerezo es una flor efímera de los cerezos que florece al comienzo de la primavera, puede ser de distintos colores pero mayormente es rosa pálido. Tiene un carácter simbólico especialmente en la cultura japonesa. Su árbol tiene una altura de seis metros, y se encuentra mayormente en la zona del hemisferio norte con climas templados",
+  },
 ];
 
 const array2 = [
   infoRespuesta2,
-  { rio:"El rio Nilo es el más largo de África con una longitud de 6,650 km y una anchura de 2,8 km es el segundo río más grande del mundo. Fluye hacia el norte a través del noreste de África para desembocar en el mar Mediterráneo." },
+  {
+    rio: "El rio Nilo es el más largo de África con una longitud de 6,650 km y una anchura de 2,8 km es el segundo río más grande del mundo. Fluye hacia el norte a través del noreste de África para desembocar en el mar Mediterráneo.",
+  },
 ];
 
 const array3 = [
   infoRespuesta3,
-  { cordillera:"La cordillera de los andes se encuentra en la zona occidental de Ámerica del Sur, cuenta con una longitud de 8.500 km y una superficie de 3.371 millones de km²"}
+  {
+    cordillera:
+      "La cordillera de los andes se encuentra en la zona occidental de Ámerica del Sur, cuenta con una longitud de 8.500 km y una superficie de 3.371 millones de km²",
+  },
 ];
 
-function incorrecta() {
-  alert(
+function incorrecta(respuesta) {
+  correcta1.textContent =
     "¡Respondiste mal!, la respuesta era " +
-      r1 +
-      ". Vamos a la siguiente pregunta"
-  );
-}
-function incorrecta2() {
-  alert(
-    "¡Buen intento, aunque es incorrecta!, la respuesta era " +
-      r2 +
-      ", acertaste " +
-      contador
-  );
-}
-function incorrecta3() {
-  alert(
-    "¡Buen intento, aunque es incorrecta!, la respuesta era " +
-      r3 +
-      ", acertaste " +
-      contador
-  );
-}
-function correcta() {
-  alert("¡Respondiste bien!, vamos a la siguiente pregunta");
+    respuesta +
+    ". Vamos a la siguiente pregunta";
 }
 
-let pregBienvenida = prompt(
-  "Bienvenido a un juego de preguntas, si desea jugar escriba 'quiero', sino lo desea escriba 'no quiero'"
-);
-if (pregBienvenida == "quiero") {
-  alert("Antes de empezar a jugar le pedimos que escriba las respuestas en minúsculas");
-  p1 = prompt("La primera pregunta es. ¿Cuál es la flor nacional de Japón?");
-  if(p1 == r1){
-    correcta();
-    contador++;
-  }else{
-    incorrecta();
-  }
-  p2 = prompt("La segunda pregunta es, ¿Cuál es el río más largo del mundo?");
-    if (p2 == r2) {
+function correcta() {
+  correcta1.textContent = "¡Respondiste bien!, vamos a la siguiente pregunta";
+}
+
+function correctaFinal() {
+  correcta2.textContent =
+    "Terminaste con una puntuacion de " + contador + " , felicidades.";
+}
+
+txt.textContent =
+  "Bienvenido a un juego de preguntas, si desea jugar escriba 'quiero', sino lo desea escriba 'no quiero'";
+boton.onclick = () => {
+  if (input.value.toLowerCase() == "quiero") {
+    txt.textContent =
+      "La primera pregunta es. ¿Cuál es la flor nacional de Japón?";
+    input.value = "";
+    boton.id = "boton1";
+    const boton1 = document.getElementById("boton1");
+    boton1.onclick = () => {
+      if (input.value.toLowerCase() == r1) {
         correcta();
         contador++;
-  }else{
-    incorrecta2();
+      } else {
+        incorrecta(r1);
+      }
+      const siguiente = document.getElementById("siguiente");
+      siguiente.classList.replace("d-none", "d-block");
+      siguiente.onclick = () => {
+        correcta1.innerText = "";
+        boton.id = "boton2";
+        const boton2 = document.getElementById("boton2");
+        txt.textContent =
+          "La segunda pregunta es, ¿Cuál es el río más largo del mundo?";
+        input.value = "";
+        boton2.onclick = () => {
+          if (input.value.toLowerCase() == r2) {
+            correcta();
+            contador++;
+          } else {
+            incorrecta(r2);
+          }
+          siguiente.onclick = () => {
+            correcta1.innerText = "";
+            boton.id = "boton3";
+            const boton3 = document.getElementById("boton3");
+            txt.textContent =
+              "La tercera pregunta es, ¿Cuál es la cordillera más LARGA del mundo?";
+            input.value = "";
+            boton3.onclick = () => {
+              if (input.value.toLowerCase() == r3) {
+                correcta();
+                contador++;
+              } else {
+                incorrecta(r3);
+              }
+            };
+          };
+        };
+      };
+    };
   }
-  p3 = prompt("La tercera pregunta es, ¿Cuál es la cordillera más LARGA del mundo?");
-    if (p3 ==r3) {
-    alert("¡Respondiste bien!");
-    contador++;
-  } else{
-    incorrecta3();
-  }
-  if(contador===3){
-    alert("¡Felicidades! Ganaste la trivia de preguntas, tu puntuación fue de " +contador);
-  }else{
-    alert("¡Buen intento! Recarga la página y volve a intentarlo para conseguir una puntuación perfecta")
-  }
-  informacion = prompt("¿Querés saber más acerca de las respuestas que te presentamos? Escribi 'si' o 'no'.");
-  if (informacion == "si") {
-    let keyword=prompt("¿Sobre cual de las respuestas que te dimos querés saber más? Si elige sobre la Flor de cerezo le pedimos que escriba 'flor', si elige el Río Nilo 'rio', por otro lado si prefiere la Cordillera de los Andes escriba 'cordillera'. \n\n 1.Flor de cerezo \n2.Río Nilo \n3.Cordillera de los andes");
-    if(keyword=="flor"){
-      const filtrado=array1.filter((flor)=>flor.
-      flor.includes(keyword));
-      console.log(filtrado)
-    }if(keyword=="rio"){
-      const filtrado2=array2.filter((rio)=>rio.
-      rio.includes(keyword));
-      console.log(filtrado2)
-    }if(keyword=="cordillera"){
-      const filtrado3=array3.filter((cordillera)=>cordillera.
-      cordillera.includes(keyword));
-      console.log(filtrado3)
-    }
-  }else{
-    alert("¡Gracias por visitarnos! Vuelva pronto.")
-}
-}else{
-  alert("¡Gracias por visitarnos! Vuelva pronto.")
-}
+};
